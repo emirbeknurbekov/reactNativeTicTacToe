8c3400dad40130b15957bb6114d7296d46b7dcf7
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-10-29 20:42:16 +0600

    game is ready

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { ImageBackground } from "react-native";
+
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+    if (calculateWinner(newBoard) || newBoard[index]) return;
+
+    newBoard[index] = xIsNext ? "X" : "O";
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <TouchableOpacity
+        style={styles.square}
+        onPress={() => handleClick(index)}
+      >
+        <Text style={styles.squareText}>{board[index]}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require("./assets/bg1.jpg")}
+    >
+      <Text style={styles.status}>{status}</Text>
+      <View style={styles.board}>
+        <View style={styles.row}>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </View>
+      </View>
+      <Button
+        color="orange"
+        title="Reset"
+        onPress={() => setBoard(Array(9).fill(null))}
+      />
+    </ImageBackground>
+  );
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  status: {
+    marginBottom: 10,
+    fontSize: 24,
+  },
+  board: {
+    flexDirection: "column",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  square: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  squareText: {
+    fontSize: 24,
+  },
+});
